d788485c95a295cc9f2a69ba78e4a48cf4506698
cduwors <christinaduwors@gmail.com>
2022-09-11 14:28:03 -0400

    troubleshooting server error

--- models/House.js
+++ models/House.js
@@ -1,54 +1,56 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-Post.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      owner_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      address_1: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      address_2: {
-        type: DataTypes.STRING,
-      },
-      city: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      state: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      zip: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [5]
-        }
-      },
-      created_by_user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
-      },
-    },
-    {
-      sequelize,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'house'
-    }
-  );
-  
-  module.exports = House
+class House extends Model {}
+
+House.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		owner_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		address_1: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		address_2: {
+			type: DataTypes.STRING,
+		},
+		city: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		state: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		zip: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [5],
+			},
+		},
+		created_by_user_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: "user",
+				key: "id",
+			},
+		},
+	},
+	{
+		sequelize,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "house",
+	}
+);
+
+module.exports = House;
