f5ef4b8d43773267663d8e2c7e731ae3eb482d42
Zach Oda <zjodachow@gmail.com>
2022-09-14 13:29:54 -0400

    submit route fixed

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,13 @@ const apiRoutes = require("./api");
 const homepageRoutes = require("./homepage-routes.js");
 const viewHomes = require("./view-homes-routes.js");
 const dashboardRoutes = require("./dashboard-routes.js");
+const postHouseRoutes = require('./post-house-routes');
 
 router.use("/", homepageRoutes);
 router.use("/api", apiRoutes);
 router.use("/view-homes", viewHomes);
 router.use("/dashboard", dashboardRoutes);
+router.use('/post-house', postHouseRoutes);
 
 router.use((req, res) => {
 	res.status(404).end();
