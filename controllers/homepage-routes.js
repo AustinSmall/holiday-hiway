09e5420cb3ad50fc2ab5763f98fa8c5a0cd2d4e3
cduwors <christinaduwors@gmail.com>
2022-09-15 15:44:25 -0400

    countdown api connected

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,21 +1,55 @@
 const router = require("express").Router();
+// const https = require("https");
+
+// const days = document.querySelector("#countdownDays");
 
 router.get("/", (req, res) => {
-  res.render("homepage")
+	// getCountdown();
+	res.render("homepage");
 });
 
 router.get("/login", (req, res) => {
-  console.log(req.session);
-  if(req.session.isLoggedIn) {
-    console.log("The user is logged in");
-    res.redirect("/");
-    return;
-  }
-  res.render("login")
+	console.log(req.session);
+	if (req.session.isLoggedIn) {
+		console.log("The user is logged in");
+		res.redirect("/");
+		return;
+	}
+	res.render("login");
 });
 
 router.get("/signup", (req, res) => {
-  res.render("sign-up")
+	res.render("sign-up");
 });
 
+// const getCountdown = function () {
+// 	const apiUrl = "https://christmas-days.anvil.app/_/api/get_days";
+
+// 	console.log("made it to fetch");
+
+// 	const request = https.request(apiUrl, (response) => {
+// 		let data = "";
+// 		response.on("data", (chunk) => {
+// 			data = data + chunk.toString();
+// 		});
+
+// 		response.on("end", () => {
+// 			const body = JSON.parse(data);
+// 			const daysToXmas = body["Days to Christmas"];
+// 			days.setAttribute("src", daysToXmas);
+
+// 			console.log(body);
+// 			// console.log(daysToXmas);
+// 		});
+// 	});
+
+// 	request.on("error", (error) => {
+// 		console.log("An error", error);
+// 	});
+
+// 	request.end();
+// };
+
 module.exports = router;
+
+// module.exports = getCountdown;
